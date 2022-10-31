b511450b6ef1c7dd1becef0032fc645bc6e854b6
lybxlpsv <lybxlpsv@yahoo.com>
2022-10-31 16:16:59 +0700

    Set systemUiVisibility during onResume for A11

--- app/src/main/java/emu/skyline/EmulationActivity.kt
+++ app/src/main/java/emu/skyline/EmulationActivity.kt
@@ -303,6 +303,16 @@ class EmulationActivity : AppCompatActivity(), SurfaceHolder.Callback, View.OnTo
         }
 
         changeAudioStatus(true)
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+       }
     }
 
     /**
