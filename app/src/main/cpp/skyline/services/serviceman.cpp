c7b4b4e17a9600cf458290a3651692b4e14ce364
Dima <dimaxdqwerty@gmail.com>
2022-11-04 15:18:40 +0300

    Stub caps:su

--- app/src/main/cpp/skyline/services/serviceman.cpp
+++ app/src/main/cpp/skyline/services/serviceman.cpp
@@ -53,6 +53,7 @@
 #include "capsrv/IAlbumAccessorService.h"
 #include "capsrv/ICaptureControllerService.h"
 #include "capsrv/IAlbumApplicationService.h"
+#include "capsrv/IScreenShotApplicationService.h"
 #include "serviceman.h"
 
 #define SERVICE_CASE(class, name, ...) \
@@ -132,6 +133,7 @@ namespace skyline::service {
             SERVICE_CASE(capsrv::IAlbumAccessorService, "caps:a")
             SERVICE_CASE(capsrv::ICaptureControllerService, "caps:c")
             SERVICE_CASE(capsrv::IAlbumApplicationService, "caps:u")
+            SERVICE_CASE(capsrv::IScreenShotApplicationService, "caps:su")
             SERVICE_CASE(nim::IShopServiceAccessServerInterface, "nim:eca")
             SERVICE_CASE(ro::IRoInterface, "ldr:ro")
             default:
